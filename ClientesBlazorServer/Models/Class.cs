40ca5ce405711a96656962f2120ae374771c8585
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-06 16:01:28 -0400

    Creacion del modelo con los sigtes atributos: ClienteId, Nombres, Telefono, Celular, Rnc, Email, Direccion

---
+++ ClientesBlazorServer/Models/Class.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ClientesBlazorServer.Models
+{
+    public class Class
+    {
+        //ClienteId, Nombres, Telefono, Celular, Rnc, Email, Direccion
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombres es obligatorio.")]
+        public string? Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo Telefono es obligatorio.")]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "El campo Teléfono debe tener el formato XXX-XXX-XXXX.")]
+        public string? Telefono { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Celular es obligatorio.")]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "El campo Celular debe tener el formato XXX-XXX-XXXX.")]
+        public string? Celular { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Rnc es obligatorio.")]
+        public string? Rnc { get; set; }
+
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El campo Email no es una dirección de correo electrónico válida.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "El campo Direccion es obligatorio.")]
+        [StringLength(200, ErrorMessage = "El campo Dirección no debe de tener más de 200 caracteres.")]
+        public string? Direccion { get; set; }
+    }
+}
