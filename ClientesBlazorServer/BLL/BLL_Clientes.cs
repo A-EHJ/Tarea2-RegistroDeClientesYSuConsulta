5b7c824c8d353cca7b3790a6917d65cd499628cc
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-06 20:39:24 -0400

    Creacion del BLL cliente con sus respectivas funciones y comprobacion del nombre y Rnc al agregar

---
+++ ClientesBlazorServer/BLL/BLL_Clientes.cs
@@ -0,0 +1,77 @@
+﻿using ClientesBlazorServer.DAL;
+using ClientesBlazorServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ClientesBlazorServer.BLL
+{
+    public class BLL_Clientes
+    {
+        private Contexto Contexto;
+
+        public BLL_Clientes(Contexto contexto)
+        {
+            Contexto = contexto;   
+        }
+
+        public bool Existe(int id)
+        {
+            return Contexto.Clientes.Any(o => o.ClienteId == id);
+        }
+        private bool Insertar(Cliente clientes)
+        {
+            Contexto.Clientes.Add(clientes);
+            return Contexto.SaveChanges() > 0;
+        }
+
+        private bool Modificar(Cliente cliente)
+        {
+            var PrioridadADesechar = Contexto.Clientes.Find(cliente.ClienteId);
+            if (cliente != null)
+            {
+                Contexto.Entry(PrioridadADesechar).State = EntityState.Detached;
+                Contexto.Entry(cliente).State = EntityState.Modified;
+                return Contexto.SaveChanges() > 0;
+            }
+            return false;
+
+        }
+
+        public bool Guardar(Cliente cliente)
+        {
+            if (Contexto.Clientes.Any(p => p.ClienteId != cliente.ClienteId && p.Rnc == cliente.Rnc || p.Nombres == cliente.Nombres))
+            {
+                return false;
+            }
+            if (!Existe(cliente.ClienteId))
+                return Insertar(cliente);
+            else
+                return Modificar(cliente);
+        }
+
+        public bool Eliminar(Cliente cliente)
+        {
+
+            
+            if (cliente != null)
+            {
+                Contexto.Entry(cliente).State = EntityState.Deleted;
+                return Contexto.SaveChanges() > 0;
+            }
+            return false;
+
+        }
+
+        public Cliente? Buscar(int id)
+        {
+            return Contexto.Clientes.Where(o => o.ClienteId == id).AsNoTracking().SingleOrDefault(); ;
+        }
+
+        public List<Cliente> GetList(Expression<Func<Cliente, bool>> criterio)
+        {
+            return Contexto.Clientes.AsNoTracking().Where(criterio).ToList();
+        }
+
+
+    }
+}
